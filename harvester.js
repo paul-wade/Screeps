c5632262d27c0e4492fbbc292c7f486938232635
Paul Wade <pwade@questarai.com>
2020-12-01 13:42:28 -0600

    deliver

--- harvester.js
+++ harvester.js
@@ -10,15 +10,23 @@
 module.exports = {
     run(creep) {
         console.log(creep);
-        const target = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
-        if (target) {
-            if (creep.harvest(target) == ERR_NOT_IN_RANGE) {
+        if (creep.store[RESOURCE_ENERGY] < creep.store.getCapacity()) {
+            const target = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
+            if (target) {
+                console.log(creep.harvest(target));
+                if (creep.harvest(target) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target);
+                }
+            }
+        }
+        else {
+            //deliver
+            const target = creep.pos.findClosestByRange(STRUCTURE_CONTROLLER);
+            console.log(creep.transfer(target, RESOURCE_ENERGY));
+            if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(target);
             }
 
         }
-    },
-    harvest() {
-
     }
 };
